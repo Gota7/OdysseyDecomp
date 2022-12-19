0b4c5e6ca7473b7adc0cea2ac01b22ea817b792f
shibbo <supershibbo12@gmail.com>
2022-12-19 14:22:18 -0500

    change setup.py to supply compiler properly

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import urllib.request
 import os
 import sys
 import subprocess
+import tarfile
 
 def install(what):
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', what])
@@ -47,6 +48,23 @@ try:
 except ModuleNotFoundError:
     print("Module 'colorama' not found. Installing...")
     install("colorama")
+
+# this code isn't tested because I don't want to download clang again, will change if errors arise
+if not os.path.exists("tools"):
+    print("Downloading Clang 3.9.1...")
+    os.mkdir("tools")
+
+    with urllib.request.urlopen("https://releases.llvm.org/3.9.1/clang+llvm-3.9.1-x86_64-linux-gnu-debian8.tar.xz") as response, open("compiler.tar.xz", "wb") as out:
+        data = response.read()
+        out.write(data)
+
+    tar_file = tarfile.open("compiler.tar.xz")
+    tar_file.extractall("tools/clang")
+    tar_file.close()
+
+    os.rename("tools/clang+llvm-3.9.1-x86_64-linux-gnu-debian8", "tools/clang")
+else:
+    print("Tools already exist, continuing...")
 
 print("Done.")
 sys.exit(1)
