88ddf2dcccefe38fa0492a9efc8a9cd355cc76a4
GRAnimated <devin@nhscan.com>
2022-12-23 21:32:28 -0500

    Remake progress.py for more accurate count

--- progress.py
+++ progress.py
@@ -1,35 +1,47 @@
-import math, os, sys
+import math, os, sys, csv
+
+TOTAL_GAME_SIZE = 0x7100A991D8 # Stop at the end of this
+
+total_size = 0 # Size of all functions that we count
+total_funcs = 0 # Amount of functions
+completed_funcs = 0 # Size of completed functions
+
+prev_address = 0
+
+# Start of the symbol for functions we want to skip
+skip_funcs = ("_ZN2nn", "_ZThn32_N2nn", "_ZN4sead")
 
 def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
     return math.trunc(stepper * number) / stepper
 
-done_size = 0
-TOTAL_GAME_SIZE = 0xA991D8
+with open('data/functions.csv', 'r') as f:
+    reader = csv.reader(f)
 
-func_sizes = { }
+    next(reader)
 
-with open("data/funcSizes.txt", "r") as f:
-    lines = f.readlines()
+    for row in reader:
+        address, symbol_name, matching = row
+        total_funcs += 1
 
-for line in lines:
-    spl = line.split("=")
-    func_sizes[spl[0]] = spl[1].replace("\n", "")
+        if symbol_name.startswith(skip_funcs):
+            continue
+        else:
+            func_size = int(address, 16) - prev_address
+            total_size += func_size
+            prev_address = int(address, 16)
+            
+            if matching == "true":
+                completed_funcs += 1
 
-with open("data/functions.csv", "r") as f:
-    csvData = f.readlines()
 
-for c in csvData:
-    spl = c.split(",")
-    isDone = spl[2].replace("\n", "")
-    
-    if isDone == "true":
-        funcSize = func_sizes[spl[1]]
-        done_size = done_size + int(funcSize)
+        if total_size >= TOTAL_GAME_SIZE:
+            break
 
-prog = (done_size / TOTAL_GAME_SIZE) * 100.0
+prog = (completed_funcs / total_funcs) * 100
+
 print("Progress:")
-print(f"{prog}% [{done_size} / {TOTAL_GAME_SIZE}]")
+print(f"{prog:.5f}% [{completed_funcs} / {total_funcs}]")
 
 print("Generating JSON...")
 
